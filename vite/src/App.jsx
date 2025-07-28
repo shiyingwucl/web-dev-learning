f0400de37a86beb7f82baa86674c1237a00f3b5d
shiying <shiying.wei@ucl.ac.uk>
2025-07-28 09:54:43 +0100

    Session three on vite/react

---
+++ vite/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [text,setText] = useState("test")
+
+  if (text == "test"){
+    return (
+      <div>Test</div>
+    )
+  }
+  return (
+    <>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
